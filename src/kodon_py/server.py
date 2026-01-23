e1a8e1290db7802f3ed7b91cc07a1815f7c1d93e
Charles Pletcher <cpletcher2@gmail.com>
2026-01-23 09:29:52 -0500

    Fix database initialization

--- src/kodon_py/server.py
+++ src/kodon_py/server.py
@@ -1,14 +1,102 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_alembic import Alembic
+from flask_sqlalchemy_lite import SQLAlchemy
+
+from typing import Any, List, Optional
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.types import JSON
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    relationship,
+)
+
+
+class Model(DeclarativeBase):
+    type_annotation_map = {dict[str, Any]: JSON}
+
+
+db = SQLAlchemy()
+
+alembic = Alembic(metadatas=Model.metadata)
+
+
+class Document(Model):
+    __tablename__ = "documents"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    editionStmt: Mapped[str]
+    language: Mapped[str]
+    publicationStmt: Mapped[str]
+    respStmt: Mapped[str]
+    sourceDesc: Mapped[str]
+    textgroup: Mapped[str]
+    textparts: Mapped[List["Textpart"]] = relationship(back_populates="document")
+    title: Mapped[str]
+    urn: Mapped[str] = mapped_column(unique=True)
+
+
+class Element(Model):
+    __tablename__ = "elements"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    attributes: Mapped[Optional[dict[str, Any]]]
+    idx: Mapped[int]
+    parent: Mapped[Optional["Element"]] = relationship(back_populates="elements")
+    parent_id: Mapped[Optional[int]] = mapped_column(ForeignKey("elements.id"))
+    tagname: Mapped[str]
+    textpart: Mapped["Textpart"] = relationship(back_populates="elements")
+    textpart_id: Mapped[int] = mapped_column(ForeignKey("textparts.id"))
+    tokens: Mapped[List["Token"]] = relationship(back_populates="element")
+    urn: Mapped[str]
+
+
+class Textpart(Model):
+    __tablename__ = "textparts"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    document: Mapped["Document"] = relationship(back_populates="textparts")
+    document_urn: Mapped[str] = mapped_column(ForeignKey("documents.urn"))
+    elements: Mapped[List["Element"]] = relationship(back_populates="textpart")
+    idx: Mapped[int]
+    location: Mapped[Optional[str]]
+    n: Mapped[Optional[str]]
+    subtype: Mapped[Optional[str]]
+    type: Mapped[Optional[str]]
+    urn: Mapped[str] = mapped_column(unique=True)
+
+
+class Token(Model):
+    __tablename__ = "tokens"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    element: Mapped["Element"] = relationship(back_populates="tokens")
+    element_id: Mapped[int] = mapped_column(ForeignKey("elements.id"))
+    position: Mapped[int]
+    text: Mapped[str]
+    textpart: Mapped["Textpart"] = relationship(back_populates="tokens")
+    textpart_id: Mapped[int] = mapped_column(ForeignKey("textparts.id"))
+    urn: Mapped[str]
+    whitespace: Mapped[bool]
+
+
+def run_migrations(app):
+    """Run database migrations to head using alembic commands API."""
+    with app.app_context():
+        alembic.upgrade()
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY="dev",
-        DATABASE_PATH=os.path.join(app.instance_path, "kodon-db.sqlite"),
+        SECRET_KEY=os.getenv("FLASK_APP_SECRET_KEY", "dev"),
+        SQLALCHEMY_ENGINES={
+            "default": f"sqlite:///{os.path.join(app.instance_path, "kodon-db.sqlite")}"
+        },
     )
 
     if test_config is None:
@@ -24,19 +112,17 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    db.init_app(app)
+    alembic.init_app(app)
+    run_migrations(app)
+
     # a simple page that says hello
     @app.route("/hello")
     def hello():
         return "Hello, World!"
 
-    alembic = Alembic()
-    alembic.init_app(app)
-
-    @app.teardown_appcontext
-    def shutdown_session(exception=None):
-        from kodon_py import database
-
-        db_session = database.init_db(app.config["DATABASE_PATH"])
-        db_session.remove()
+    @app.route("/<urn>")
+    def passage(urn=None):
+        return render_template("ReadingEnvironment.html.jinja", urn=urn)
 
     return app
